c3b9050feefca1c205f3740a56de12f3049145c2
dan beecroft <binauralbeat001@gmail.com>
2018-09-01 10:07:48 -0500

    initial commit

--- yak_tracker/urls.py
+++ yak_tracker/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include('website.urls')),
+
 ]
