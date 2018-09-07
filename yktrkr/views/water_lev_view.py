34bce6e3c1b8b12cebcd79f4067b4012a5543951
dan beecroft <binauralbeat001@gmail.com>
2018-09-06 19:05:51 -0500

    have river data coming in from USGS api

--- yktrkr/views/water_lev_view.py
+++ yktrkr/views/water_lev_view.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import RequestContext
 import requests
+import json
 
 
 def water_levels(request):
     response = requests.get('http://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&stateCd=tn&parameterCd=00060,00065&siteType=ST&siteStatus=all')
     level_data_raw = response.json()
-    level_data = str(level_data_raw['value']['timeSeries']['sourceInfo']['siteName'])
-    site_name = level_data
+    level_data = (level_data_raw['value']['timeSeries'][0]['sourceInfo']['siteName'])
+
+    site_name = str(level_data)
 
 
     return render(request, 'levels.html', {'site_name': site_name})
